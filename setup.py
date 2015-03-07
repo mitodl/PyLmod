2cf66f740bb9d1dd105489365138054922cb038f
pwilkins <pwilkins@mit.edu>
2015-03-06 21:14:21 -0500

    API doc complete save for attribute comments

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import sys
 
 from setuptools import setup, find_packages
 from setuptools.command.test import test as testcommand
-from pylmod import __version__
+
+VERSION = __import__('pylmod').VERSION
 
 with open('test_requirements.txt') as test_reqs:
     tests_require = test_reqs.readlines(),
