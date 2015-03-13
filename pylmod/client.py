7236122df70d78465764f193bf563d6862d0ff4a
pwilkins <pwilkins@mit.edu>
2015-03-13 10:31:48 -0400

    attempt to debug RTD version error

--- pylmod/client.py
+++ pylmod/client.py
@@ -14,16 +14,20 @@ class Client(GradeBook, Membership):  # pylint: disable=too-few-public-methods
     Use Client class to incorporate multiple Learning Modules APIs.
 
     Example usage:
-    gradebook = Client('ichuang-cert.pem')
-    ats = gradebook.get('academicterms')
-    tc = ats['data'][0]['termCode']
-    gradebook.get('academicterm',termCode=tc)
-    students = gradebook.get_students()
-    assignments = gradebook.get_assignments()
-    gradebook.create_assignment('midterm1', 'mid1', 1.0, 100.0, '11-04-2013')
-    sid, student = gradebook.get_student_by_email(email)
-    aid, assignment = gradebook.get_assignment_by_name('midterm1')
-    gradebook.set_grade(aid, sid, 95.2)
-    gradebook.spreadsheet2gradebook(datafn)
+
+    .. code-block:: python
+
+        gradebook = Client('ichuang-cert.pem')
+        ats = gradebook.get('academicterms')
+        tc = ats['data'][0]['termCode']
+        gradebook.get('academicterm',termCode=tc)
+        students = gradebook.get_students()
+        assignments = gradebook.get_assignments()
+        gradebook.create_assignment('midterm1', 'mid1', 1.0,
+                                    100.0, '11-04-2013')
+        sid, student = gradebook.get_student_by_email(email)
+        aid, assignment = gradebook.get_assignment_by_name('midterm1')
+        gradebook.set_grade(aid, sid, 95.2)
+        gradebook.spreadsheet2gradebook(datafn)
     """
     pass
