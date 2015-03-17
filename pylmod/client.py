13928cc91ea081804f0775e868660db3e678c4fd
pwilkins <pwilkins@mit.edu>
2015-03-17 16:51:48 -0400

    respond to PR comments for API doc, still more comments to address

--- pylmod/client.py
+++ pylmod/client.py
@@ -18,16 +18,17 @@ class Client(GradeBook, Membership):  # pylint: disable=too-few-public-methods
     .. code-block:: python
 
         gradebook = Client('ichuang-cert.pem')
-        ats = gradebook.get('academicterms')
-        tc = ats['data'][0]['termCode']
-        gradebook.get('academicterm',termCode=tc)
+        academic_terms = gradebook.get('academicterms')
+        term_code = academic_terms['data'][0]['termCode']
+        gradebook.get('academicterm',termCode=term_code)
         students = gradebook.get_students()
         assignments = gradebook.get_assignments()
         gradebook.create_assignment('midterm1', 'mid1', 1.0,
                                     100.0, '11-04-2013')
-        sid, student = gradebook.get_student_by_email(email)
-        aid, assignment = gradebook.get_assignment_by_name('midterm1')
-        gradebook.set_grade(aid, sid, 95.2)
-        gradebook.spreadsheet2gradebook(datafn)
+        student_id, student = gradebook.get_student_by_email(email)
+        assignment_id, assignment =
+            gradebook.get_assignment_by_name('midterm1')
+        gradebook.set_grade(assignment_id, student_id, 95.2)
+        gradebook.spreadsheet2gradebook(csv_file)
     """
     pass
